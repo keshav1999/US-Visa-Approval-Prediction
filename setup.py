712d7c8facdf0867b7faa48910c6c479504df8b3
keshav <keshavc24@gmail.com>
2024-06-21 15:58:42 +0530

    requirement

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Keshav",
+    author_email="keshavc24@gmail.com",
+    packages=find_packages()
+)
